4481ae920ec20b6fe8bb160a83b661e0df826b3f
Arleen Silva <arleensilva20@gmail.com>
2018-11-23 01:16:43 -0200

    Iniciado parser

--- src/CodeParser.js
+++ src/CodeParser.js
@@ -1,47 +1,62 @@
-// import React, { Component } from "react";
-
-// class CodeParser extends Component {
-//     constructor(props){
-//         super(props)
-//         this.state = { message: 'ainda n' }
-
-//         this.handleMessage = this.handleMessage.bind(this)
-//         this.handleCode = this.handleCode.bind(this)
-//     }
-
-//     render(){
-//         let code = this.props.code;
-//         this.handleCode(code);
-
-//         return(
-//             <div>
-//                 <p>{this.state.message}</p>
-//             </div>
-//         )
-//     }
-
-//     handleMessage(e){
-//         //this.setState( {...this.state, message: e })
-//         this.state.message = e
-//     }
-
-//     handleCode(code){
-//         if(code == '1'){
-//             this.handleMessage('número 1')
-//         } else {
-//             this.handleMessage('n é número 1')
-//         }
-//     }
-// }
-
-// export default CodeParser
-
 export default (code) => {
-    if(code == '1'){
-        return 'número 1' + a
-    } else {
-        return 'n é número 1' + a
+    let lines = code.split('\n');
+    let message='';
+    let state=0;
+
+    function isLetter(str){
+        let regex = /^[A-Za-z]+$/
+        return str.length == 1 && regex.test(str)
     }
+    
+    function isNumber(str){
+        let regex = /^[0-9]+$/
+        return str.length == 1 && regex.test(str)
+    }
+
+    function isSpace(str){
+        let regex = /^\s+$/
+        return str.length == 1 && regex.test(str)
+    }
+
+    lines.forEach(line => {
+        
+        for(let i=0; i<line.length; i++){
+            let x = line.charAt(i);
+
+            if(isSpace(x)) state = 0
+            else if(x == '/') state = 1
+            else if(isLetter(x)) state = 5
+            else state = -1
+
+            switch(state){
+                case 0:
+                    if(isSpace(x)) state = 0
+                    else if(x == '/') state = 1
+                    else if(isLetter(x)) state = 5
+                    else state = -1
+                    message = 'Compilação OK'
+                break;
+
+                case 1:
+                    message = '/ verificada'
+                break;
+
+                case 5:
+                    message = 'letra verificada'
+                break;
+
+                case -1:
+                    message = 'Erro Compilação'
+                break
+            }
+
+            if(state == -1) break
+        }
+    })
+
+    return message
 }
 
-let a = 3
+
+//message += `posição ${i} e char ${line.charAt(i)} é letra ${isLetter(line.charAt(i))} e é número ${isNumber(line.charAt(i))}     | | | `
+//console.log(isSpace(line.charAt))
